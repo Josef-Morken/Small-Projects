9253411729d0afd64f5e7b48ce714dc1fd1e1880
Josef Morken <22643576+Josef-Morken@users.noreply.github.com>
2023-03-09 13:12:55 -0500

    Add files via upload

---
+++ Simple Banking App/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Account josef = new Account("Josef Morken", "SBA0000");
+        josef.printMenu();
+
+        AccountV2 Jenn = new AccountV2("Josef Morken", "SBA0000");
+        Jenn.printMenu();
+
+
+    }
+}
